da560d35caac018daab37c268acfff0982e901c4
MPSB\shafiq.irwan <shafiq.irwan@quickash.com>
2024-08-19 16:00:03 +0800

    Commit Changes

--- game/game_manager.gd
+++ game/game_manager.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var HIT_POINTS = 0
+var DASH_TIMER = 0.5
--- game/player.gd
+++ game/player.gd
@@ -2,10 +2,16 @@ extends CharacterBody2D
 
 signal health_depleted
 var health = 100.0
+var dash_speed = 2000
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
+	
+	if(Input.is_action_just_pressed("dash")):
+		velocity = direction * dash_speed
+		ghosting()
+	
 	move_and_slide()
 	
 	if velocity.length() > 0.00:
@@ -21,3 +27,8 @@ func _physics_process(delta):
 		%ProgressBar.value = health
 		if health <= 0.0:
 			health_depleted.emit()
+			
+func ghosting():
+	var tween_fade = get_tree().create_tween()
+	tween_fade.tween_property(self, "self_modulate", Color(1,1,1,1), 0.75)
+	await tween_fade.finished
