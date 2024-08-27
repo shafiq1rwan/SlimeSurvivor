4baf98e3960f05881e1b2506d6a64cb241dbc2c5
MPSB\shafiq.irwan <shafiq.irwan@quickash.com>
2024-08-27 17:34:31 +0800

    Add SFX and Gun follows Mouse Cursor

--- game/pistol.gd
+++ game/pistol.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 func _physics_process(delta):
-	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range.front()
-		look_at(target_enemy.global_position)
+	#var enemies_in_range = get_overlapping_bodies()
+	#if enemies_in_range.size() > 0:
+		#var target_enemy = enemies_in_range.front()
+		#look_at(target_enemy.global_position)
+	
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func shoot():
 	const BULLET = preload("res://game/bullet.tscn")
@@ -14,6 +19,7 @@ func shoot():
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
+	$SFXShoot.play()
 
-func _on_timer_timeout():
-	shoot()
+#func _on_timer_timeout():
+	#shoot()
